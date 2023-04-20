bd5b2c3cd3c089c21c8c7bf547106f5c51918f79
bukhantcev <bukhantcev@mail.ru>
2023-04-21 01:03:35 +0300

    Lesson2

---
+++ src/main/java/org/gb/Plate.java
@@ -0,0 +1,25 @@
+package org.gb;
+
+public class Plate {
+    private int food;
+
+    public int getFood() {
+        return food;
+    }
+
+    public void setFood(int food) {
+       if(food >= 0) {
+           this.food = food;
+       }
+
+
+
+    }
+
+    public Plate(int food) {
+        this.food = food;
+    }
+    public void info() {
+        System.out.println("В тарелке " + food + " кусков еды");
+    }
+}
